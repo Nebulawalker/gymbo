6861ccdcd780f08a555290273968e125cefb40e8
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-08-14 02:55:58 +0300

    Почистил код

--- pages/models.py
+++ pages/models.py
@@ -17,9 +17,10 @@ class ContentBlock(models.Model):
         default=0,
         editable=False
     )
+
     def __str__(self) -> str:
         return self.title
-    
+
     class Meta:
         verbose_name = 'Контент-блок'
         verbose_name_plural = 'Контент-блоки'
@@ -29,7 +30,7 @@ class Page(models.Model):
     title = models.CharField(
         verbose_name='Заголовок страницы',
         max_length=200
-    ) 
+    )
     slug = models.SlugField(
         verbose_name='Название в виде url',
         max_length=200,
@@ -41,17 +42,18 @@ class Page(models.Model):
         verbose_name='Контент-блоки',
         related_name='content_blocks'
     )
+
     def __str__(self) -> str:
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse('page_detail', kwargs={'slug': self.slug})
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
-    
+
     class Meta:
         verbose_name = 'Страница'
         verbose_name_plural = 'Страницы'
