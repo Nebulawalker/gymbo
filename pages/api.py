e6d73ed48b645fbe23ead47a20fc1064c81b7a91
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-08-16 02:03:09 +0300

    Переписаны api: рефакторинг PagesAPIView, переписан PageDetailView с учетом изменений в БД

--- pages/api.py
+++ pages/api.py
@@ -1,24 +1,19 @@
-from .models import Page, ContentBlock
+from .models import Page, Impression
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import ContentBlockSerializer
+from .serializers import ContentBlockSerializer, ContentBlock
 
 
 class PagesAPIView(APIView):
     def get(self, request):
-        content_blocks = ContentBlock.objects.all()
         result = []
-        for content_block in content_blocks:
-            pages = [
-                request.build_absolute_uri(
-                    slug.get_absolute_url()
-                ) for slug in Page.objects.filter(
-                                  content_blocks=content_block
-                              )
-            ]
+        pages = Page.objects.all()
+        for page in pages:
+            page_url = request.build_absolute_uri(page.get_absolute_url())
+            content_blocks = page.content_blocks.values_list('title', flat=True)
             result.append({
-                'content_block_title': content_block.title,
-                'page_urls': pages
+                'page_url': page_url,
+                'content_blocks': content_blocks
             })
         return Response(result)
 
@@ -27,7 +22,21 @@ class PageDetailView(APIView):
     def get(self, request, slug):
         page = Page.objects.get(slug=slug)
         content_blocks = [block for block in page.content_blocks.all()]
+        result = []
         for content_block in content_blocks:
-            content_block.views += 1
-            content_block.save()
-        return Response(ContentBlockSerializer(content_blocks, many=True).data)
+            impressions, is_created = Impression.objects.get_or_create(
+                page=page,
+                content_block=content_block
+            )
+            impressions.quantity += 1
+            impressions.save()
+            result.append(
+                ContentBlock(
+                    content_block.id,
+                    content_block.title,
+                    content_block.video_url,
+                    content_block.impression.filter(
+                        page=page
+                    ).values_list('quantity', flat=True)[0])
+            )
+        return Response(ContentBlockSerializer(result, many=True).data)
