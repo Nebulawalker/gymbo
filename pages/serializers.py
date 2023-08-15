97876b3365f15e692a79d873ddd482d0b4019356
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-08-16 02:02:24 +0300

    Добавлен сериализатор для контент-блоков

--- pages/serializers.py
+++ pages/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import ContentBlock
 
 
-class ContentBlockSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContentBlock
-        fields = '__all__'
+class ContentBlock:
+    def __init__(self, id, title, video_url, impressions_quantity):
+        self.id = id
+        self.title = title
+        self.video_url = video_url
+        self.impressions_quantity = impressions_quantity
+
+
+class ContentBlockSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=200)
+    video_url = serializers.URLField(max_length=200)
+    impressions_quantity = serializers.IntegerField()
